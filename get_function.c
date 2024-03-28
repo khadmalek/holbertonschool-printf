8e82fdb4046d25516ec3bd3a70d35012c5a4bc7a
khadmalek <k.abdelmalek@live.fr>
2024-03-28 20:41:19 +0100

    add get function

--- get_function.c
+++ get_function.c
@@ -9,11 +9,11 @@ int get_function(const char specifier, va_list arguments)
 {
 	int index_array = 0;
 	format_handler array[] = {
-		{'c', print_character},
+		{'c', print_char},
 		{'s', print_string},
-		{'%', print_percentage},
-		{'d', print_integer},
-		{'i', print_integer},
+		{'%', print_perc},
+		{'d', print_digit},
+		{'i', print_digit},
 		{'\0', NULL}
 	};
 	while (array[index_array].specifier != '\0')
