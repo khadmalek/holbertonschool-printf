88a773dcd96b98e512a129f591bc5b1fde415cd2
khadmalek <k.abdelmalek@live.fr>
2024-03-28 20:33:15 +0100

    grind code

--- _print_digit.c
+++ _print_digit.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * print_digit - Imprime des entiers.
- *
- * @args: Argument.
- *
- * Return: Nombre de caractères.
- */
 
 int print_digit(va_list args)
 {
-int count = 0;
-long int digit = va_arg(args, int);
-int is_negative = 0;
-if (digit < 0)
-{
-is_negative = 1;
-digit = -digit;
-}
-if (digit == 0)
-{
-return (_putchar('0'));
+    int is_negative = 0;
+    long int digit = va_arg(args, int);
+    int count = 0;
+    int digits[10]; // Déclaration du tableau ici
+    int index = 0; // Déclaration de la variable 'index' ici
+    int i;
+
+    if (digit < 0)
+    {
+        is_negative = 1;
+        digit = -digit;
+    }
+
+    if (digit == 0)
+    {
+        return _putchar('0');
+    }
+
+    while (digit != 0)
+    {
+        int current_digit = digit % 10;
+        digits[index++] = current_digit;
+        digit /= 10;
+    }
+
+    if (is_negative)
+    {
+        count += _putchar('-');
+    }
+
+    for (i = index - 1; i >= 0; i--)
+    {
+        count += _putchar(digits[i] + '0');
+    }
+
+    return count;
 }
 
-while (digit != 0)
-{
-int current_digit = digit % 10;
-count += _putchar(current_digit + '0');
-digit /= 10;
-}
 
-if (is_negative)
-{
-count += _putchar('-');
-}
-return (count);
-}
 
