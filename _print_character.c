af3f0e881f198ab73b1fd1025b3e11d727d74871
khadmalek <k.abdelmalek@live.fr>
2024-03-28 20:30:57 +0100

    add function print char

---
+++ _print_character.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_char - Prints a single character to stdout.
+ * @arg: The argument list containing the character to be printed.
+ *
+ * Return: The number of characters printed (always 1).
+ */
+int print_char(va_list arg)
+{
+    char character = va_arg(arg, int); // Correction de l'utilisation de va_arg
+
+    _putchar(character);
+
+    return (1);
+}
